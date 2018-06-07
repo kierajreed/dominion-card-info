5492f87455ffdb49731d73f3eddeb68cd89feb1c
SkyHawkB <danielmey626@gmail.com>
2018-06-07 18:20:32 -0400

    rewrite the main file

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Dominion Card Info
 
-A discord bot for info about Dominion cards.
-
-Help can be found on [the wiki](https://github.com/SkyHawkB/basic-bot/wiki/HELP).
-
-Feel free to [open pull requests](https://github.com/SkyHawkB/basic-bot/pulls), [submit issues](https://github.com/SkyHawkB/basic-bot/issues), or [just chat](https://discord.gg/AqTDZEb).
+A discord bot for information about Dominion cards.
---
+++ src/index.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json');
+const cards = require('./cardtexts.json');
+
+const request = require('request');
+const fs = require('fs');
+
+
+client.on('error', (error) => {});
+client.once('ready', () => {
+  console.log('Connected to Discord!');
+});
+
+client.on('message', (message) => {
+  if(message.author.bot || message.content.indexOf(config.prefix) !== 0) {
+    return;
+  }
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if(command.includes('/') || command.includes('\\')) {
+    return;
+  }
+
+  if(fs.existsSync(`./commands/${command}.js`)) {
+    try {
+      const commandFile = require(`./commands/${command}.js`);
+
+      commandFile.run(client, message, args);
+    } catch(e) {}
+  }
+});
