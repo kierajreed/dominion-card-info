62dc4b295d8dd9c9f3c1b1c8e6798e3a23ede632
SkyHawkB <danielmey626@gmail.com>
2018-06-10 20:24:19 -0400

    starting to re-add cards

---
+++ src/commands/wiki.js
@@ -0,0 +1,9 @@
+module.exports.run = (client, message, args) => {
+  message.channel.send('`muenstercheese` is making the code better. This command will be available soon!');
+};
+
+module.exports.info = {
+  name: '',
+  description: '',
+  usage: ''
+};
